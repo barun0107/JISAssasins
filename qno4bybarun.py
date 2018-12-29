b29c69797dcd1604332bf17c5d9caeb9b45ee50a
barun0107 <46235884+barun0107@users.noreply.github.com>
2018-12-29 15:57:06 +0530

    Add files via upload

---
+++ qno4bybarun.py
@@ -0,0 +1,15 @@
+class MaxMin:
+    def MaxandMin(self, list):
+        z = min(list)
+        y = max(list)
+        print("{} is minimum and {} is maximum".format(z, y))
+
+
+m = MaxMin()
+list = []
+n = int(input("Enter how many numbers to input:"))
+while (n > 0):
+    a = int(input())
+    list.append(a)
+    n -= 1
+m.MaxandMin(list)
